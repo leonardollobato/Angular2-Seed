06a7ee66f5275a37c66071d4aa916e9e0535cbc9
Leonardo Lobato <leonardo.lobato@gmail.com>
2016-03-18 23:57:14 -0300

    Initial Commit

---
+++ tools/config/project.config.ts
@@ -0,0 +1,36 @@
+import {join} from 'path';
+import {SeedConfig, normalizeDependencies} from './seed.config';
+
+export class ProjectConfig extends SeedConfig {
+  PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+
+  constructor() {
+    super();
+    // this.APP_TITLE = 'Put name of your app here';
+    let additional_deps: Array<any> = [
+      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      // {src: 'lodash/lodash.min.js', inject: 'libs'},
+      {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      {src: 'bootstrap/dist/js/bootstrap.min.js', inject: 'libs'},
+      {src: 'bootstrap/dist/css/bootstrap.min.css', inject: true},
+    ];
+
+    this.DEV_NPM_DEPENDENCIES = this.DEV_DEPENDENCIES.concat(normalizeDependencies(additional_deps));
+    this.PROD_NPM_DEPENDENCIES = this.PROD_NPM_DEPENDENCIES.concat(normalizeDependencies(additional_deps));
+
+    this.CSS_PROD_BUNDLE = 'main.css';
+    this.APP_ASSETS = [
+      // {src: `${this.ASSETS_SRC}/css/toastr.min.css`, inject: true},
+      // {src: `${this.APP_DEST}/assets/scss/global.css`, inject: true},
+      { src: `${this.ASSETS_SRC}/main.scss`, inject: true },
+    ];
+
+    this.DEV_DEPENDENCIES = this.DEV_NPM_DEPENDENCIES.concat(this.APP_ASSETS);
+    this.PROD_DEPENDENCIES = this.PROD_NPM_DEPENDENCIES.concat(this.APP_ASSETS);
+
+    this.FONTS_DEST = `${this.APP_DEST}/fonts`;
+    this.FONTS_SRC = [
+      'node_modules/bootstrap/dist/fonts/**'
+    ];
+  }
+}
---
+++ tools/tasks/project/build.index.dev.ts
@@ -0,0 +1,56 @@
+import * as gulp from 'gulp';
+import * as gulpLoadPlugins from 'gulp-load-plugins';
+import {join} from 'path';
+import * as slash from 'slash';
+import {APP_SRC, APP_DEST, APP_BASE, DEV_DEPENDENCIES, CSS_DEST, ASSETS_SRC} from '../../config';
+import {templateLocals} from '../../utils';
+const plugins = <any>gulpLoadPlugins();
+
+export = () => {
+  return gulp.src(join(APP_SRC, 'index.html'))
+  .pipe(inject('shims'))
+  .pipe(inject('libs'))
+  .pipe(inject())
+  .pipe(plugins.template(templateLocals()))
+  .pipe(gulp.dest(APP_DEST));
+};
+
+
+function inject(name?: string) {
+  return plugins.inject(gulp.src(getInjectablesDependenciesRef(name), { read: false }), {
+    name,
+    transform: transformPath()
+  });
+}
+
+// interact through dep. paths (lib and assets)
+function getInjectablesDependenciesRef(name?: string) {
+  return DEV_DEPENDENCIES
+  .filter(dep => dep['inject'] && dep['inject'] === (name || true))
+  .map(mapPath);
+}
+
+// if the dependency path has /src it will remove it and add to dest path
+function mapPath(dep: any) {
+  let envPath = dep.src;
+
+  if (envPath.startsWith(APP_SRC) && !envPath.endsWith('.scss')) {
+    envPath = join(APP_DEST, dep.src.replace(APP_SRC, ''));
+  }
+
+  if (envPath.startsWith(APP_SRC) && envPath.endsWith('.scss')) {
+    envPath = envPath
+    .replace(ASSETS_SRC, CSS_DEST)
+    .replace('.scss', '.css');
+  }
+
+  return envPath;
+}
+
+// insert a timestamp to avoid cache on the assets
+function transformPath() {
+  return function (filepath: string) {
+    arguments[0] = join(APP_BASE, filepath) + `?${Date.now()}`;
+    return slash(plugins.inject.transform.apply(plugins.inject.transform, arguments));
+  };
+}
